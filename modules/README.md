1b21a800e7fdc39ab30483f2cda46e414088814c
zen_ventzi <ventzislav.marinov@gmail.com>
2021-05-31 17:34:59 +0300

    add chakra-ui

---
+++ modules/README.md
@@ -0,0 +1,3 @@
+Modules are a reflection of pages folder
+
+common folder is for code that is used across many pages
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,18 @@
+import { Box } from '@chakra-ui/react';
+
 const Index = () => {
-  return <div>Index</div>;
+  return (
+    <div>
+      Index
+      <Box
+        m={2}
+        color="gray.50"
+        bgGradient="radial(gray.300,yellow.400,pink.200)"
+      >
+        Tomato
+      </Box>
+    </div>
+  );
 };
 
 export default Index;
