5637dac82b40c956188aa728900eb159b5f41345
zen_ventzi <ventzislav.marinov@gmail.com>
2021-05-31 18:33:00 +0300

    add basic grid layout in Layout.tsx

--- modules/common/Layout.tsx
+++ modules/common/Layout.tsx
@@ -1,13 +1,18 @@
+import { Grid, GridItem } from '@chakra-ui/react';
+
 type LayoutProps = {
   children: React.ReactNode;
 };
 
 const Layout = ({ children }: LayoutProps) => {
   return (
-    <div>
-      Layout <br />
-      {children}
-    </div>
+    <Grid h="100vh" templateRows="1fr" templateColumns="20% 80%">
+      <GridItem rowSpan={1} colSpan={1} bg="tomato"></GridItem>
+      <GridItem colSpan={1} bg="papayawhip">
+        Layout <br />
+        {children}
+      </GridItem>
+    </Grid>
   );
 };
 
