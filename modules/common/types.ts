8dd53825cd43dd8b98110e614a3530b21a299fea
zen_ventzi <ventzislav.marinov@gmail.com>
2021-05-31 22:48:13 +0300

    get blog posts from contentful

--- modules/common/types.ts
+++ modules/common/types.ts
@@ -1,5 +1,7 @@
 export type Post = {
+  id: string;
   title: string;
   contentPreview: string;
   content: string;
+  slug: string;
 };
--- pages/blog/[slug].tsx
+++ pages/blog/[slug].tsx
@@ -1,6 +1,13 @@
 import { Box, Center, Heading } from '@chakra-ui/layout';
+import * as contentful from 'contentful';
 import { GetStaticPaths, GetStaticProps } from 'next';
 import { Post } from '../../modules/common';
+
+const contentfulClient = contentful.createClient({
+  // FIXME
+  space: process.env.CONTENTFUL_SPACE_ID as string,
+  accessToken: process.env.CONTENTFUL_ACCESS_TOKEN as string,
+});
 
 type PostProps = {
   post: Post;
@@ -22,12 +29,38 @@ const PostPage = ({ post }: PostProps) => {
 export default PostPage;
 
 export const getStaticProps: GetStaticProps<PostProps> = async (context) => {
-  const post: Post = {
-    title: 'Titlee',
-    content:
-      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries",
-    contentPreview: 'ContentPreeview',
-  };
+  // console.log(context.params);
+
+  if (!context?.params?.slug) {
+    throw Error(`Slug not available`);
+  }
+
+  const entry = await contentfulClient.getEntries<Post>({
+    content_type: 'blogPost',
+    'fields.slug': context.params.slug,
+  });
+
+  // console.log('entry');
+  // console.dir(entry, { depth: null });
+
+  const post: Post | null | undefined = entry.items.map((item) => ({
+    id: item.sys.id,
+    title: item.fields.title,
+    content: item.fields.content,
+    contentPreview: item.fields.contentPreview,
+    slug: item.fields.slug,
+  }))[0];
+
+  // TODO: if null, redirect or do something
+
+  // const post: Post = {
+  //   id: '',
+  //   title: 'Titlee',
+  //   slug: '',
+  //   content:
+  //     "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries",
+  //   contentPreview: 'ContentPreeview',
+  // };
 
   return {
     props: { post }, // will be passed to the page component as props
@@ -35,10 +68,16 @@ export const getStaticProps: GetStaticProps<PostProps> = async (context) => {
 };
 
 export const getStaticPaths: GetStaticPaths = async () => {
+  const res = await contentfulClient.getEntries<Post>({
+    content_type: 'blogPost',
+  });
+  // console.dir(res, { depth: null });
+  const posts = res.items.map((item) => {
+    return { ...item.fields, id: item.sys.id };
+  });
+
   return {
-    paths: [
-      { params: { slug: 'test' } }, // See the "paths" section below
-    ],
+    paths: posts.map((post) => ({ params: { slug: post.slug } })),
     fallback: false, // See the "fallback" section below
   };
 };
