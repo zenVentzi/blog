7d1613407d38a31f6d618c52e67c250dc313890c
zen_ventzi <ventzislav.marinov@gmail.com>
2021-06-02 23:40:55 +0300

    add MDXProvider in app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,17 @@
 import type { AppProps /*, AppContext */ } from 'next/app';
 import { ChakraProvider } from '@chakra-ui/react';
+import { MDXProvider } from '@mdx-js/react';
 import { Layout } from '../modules/common/layout';
+import { MDXComponents } from '../modules/common';
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
   return (
     <ChakraProvider>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
+      <MDXProvider components={MDXComponents}>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </MDXProvider>
     </ChakraProvider>
   );
 };
