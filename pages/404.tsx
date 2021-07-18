0468c0b2b896144f0ca4b13c72ae6537320ba856
zen_ventzi <ventzislav.marinov@gmail.com>
2021-07-18 21:53:34 +0300

    custom 404 page

---
+++ pages/404.tsx
@@ -0,0 +1,11 @@
+import { Text, VStack } from '@chakra-ui/react';
+
+const NotFound = () => {
+  return (
+    <VStack height="100%" justify="center" align="center">
+      <Text>404 Page not found</Text>
+    </VStack>
+  );
+};
+
+export default NotFound;
