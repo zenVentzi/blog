34a9a7feacf4bc8d73f9404058c79aba4de3933b
zen_ventzi <ventzislav.marinov@gmail.com>
2022-08-02 18:45:39 +0300

    fix: index page, previewContent unformatted links

--- modules/common/getPosts.ts
+++ modules/common/getPosts.ts
@@ -8,6 +8,44 @@ type GetPostsOptions = {
   limit?: number;
 };
 
+const getContentPreview = (content: string) => {
+  let cPreviewLength = 200;
+
+  //#region code explanation
+  // below we look for empty character in order not to cut links in the middle
+  // assumption is made that links are shorter than 120 characters
+  // if there is a link longer than 120 characters
+  // in the position between 140th character and 260th character
+  // it will be cut and it will look ugly in the preview
+  // extremely unlikely to happen though, and the penalty isn't too bad
+
+  // the other solution is to regex for links, record their positions
+  // and only cut after/before a link. This way we lose control over
+  // the previewLength in case a link is too long, or short but in our
+  // cutting position
+
+  // best approach would be to extract all links in the preview region, e.g. 200 chars
+  // remove their length from the previewLegnth calculation and add only the length
+  // of their preview texts, that's actually visible when the user is in the browser
+  //#endregion
+  for (let i = 0; i < 60; i++) {
+    let lastChar = content[cPreviewLength - i];
+    let lastCharIsEmpty = lastChar.match(/\s/);
+    if (lastCharIsEmpty) {
+      cPreviewLength -= i;
+      break;
+    }
+
+    lastChar = content[cPreviewLength + i];
+    lastCharIsEmpty = lastChar.match(/\s/);
+    if (lastCharIsEmpty) {
+      cPreviewLength += i;
+    }
+  }
+
+  return content.slice(0, cPreviewLength);
+};
+
 const getPosts = async (options?: GetPostsOptions) => {
   const contentfulClient = contentful.createClient({
     // FIXME
@@ -26,10 +64,8 @@ const getPosts = async (options?: GetPostsOptions) => {
   const unserializedPosts: UnserializedPost[] | null | undefined =
     entry.items.map((item) => {
       const { content, data } = matter(item.fields.content);
-      const showReadMoreButton =
-        content.substring(0, 200).length < content.length;
-
-      let contentPreview = content.substring(0, 200);
+      let contentPreview = getContentPreview(content);
+      const showReadMoreButton = contentPreview.length < content.length;
 
       if (showReadMoreButton) {
         contentPreview += `<a href="/blog/${data.slug}"> Read more </a>`;
